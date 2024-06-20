a7e5009b63e4c78ca7386c8666d219663a0a1788
1F10230152 <s1f102301527@iniad.org>
2024-06-20 14:29:48 +0900

    コードをもう一度整形2

--- project/calc.py
+++ project/calc.py
@@ -1,8 +1,7 @@
 def fact(n):
     if n < 0:
         raise ValueError("n must be a non-negative integer")
-    result=1
-    for i in range(1,n+1):
-        result*=i
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
     return result
-    
