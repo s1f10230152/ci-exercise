716edc2975427851486ceecd6f54d7557aa931c1
1F10230152 <s1f102301527@iniad.org>
2024-06-20 14:31:50 +0900

    コードをもう一度整形3

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -4,6 +4,5 @@ from project.calc import fact
 
 class TestTarget(unittest.TestCase):
     def test_fact_positive(self):
-        self.assertEqual(fact(1),1)
-        self.assertEqual(fact(5),120)
-        
+        self.assertEqual(fact(1), 1)
+        self.assertEqual(fact(5), 120)
