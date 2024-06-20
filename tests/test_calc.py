2d2c077565b858c2ff0a1c117f6935a63431e096
1F10230152 <s1f102301527@iniad.org>
2024-06-20 13:33:45 +0900

    Initial commit

---
+++ tests/test_calc.py
@@ -0,0 +1,7 @@
+import unittest
+from project.calc import fact
+
+class TestTarget(unittest.TestCase):
+    def test_fact_positive(self):
+        self.assertEqual(fact(1),1)
+        self.assertEqual(fact(5),120)
